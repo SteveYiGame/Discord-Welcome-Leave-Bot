156bff7f3139a12a7db57181edd0450650bee75a
SteveYi Yo <jackcooku@steveyi.net>
2020-02-18 01:53:30 +0800

    Update bot.py Add config.json

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-<<<<<<< HEAD
     print(f'{member.id}, {member}加入了伺服器!')
     channel = bot.get_channel(date['join_channel'])
     await channel.send(F'<@{member.id}> 加入了伺服器!')
@@ -23,25 +22,10 @@ async def on_member_remove(member):
     print(f'{member.id}, {member}離開了伺服器!')
     channel = bot.get_channel(date['leave_channel'])
     await channel.send(F'<@{member.id}> 離開了伺服器!')
-=======
-    print(f'{member}加入了伺服器!')
-    channel = bot.get_channel(輸入要進入的頻道ID)
-    await channel.send(F'{member.mention} 加入了伺服器!')
-
-@bot.event
-async def on_member_remove(member):
-    print(f'{member}離開了伺服器!')
-    channel = bot.get_channel(輸入要離開的頻道ID)
-    await channel.send(F'{member.mention} 離開了伺服器!')
->>>>>>> 24b43c86419c5e18744956417c127f333fc5197e
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(F'<@{ctx.author.id}> 延遲為{round(bot.latency*1000)}ms')
     print(f'{ctx.author.id},{ctx.author}要求顯示延遲!')
 
-<<<<<<< HEAD
 bot.run(date['TOKEN'])
-=======
-bot.run('在此輸入機器人的Token')
->>>>>>> 24b43c86419c5e18744956417c127f333fc5197e
